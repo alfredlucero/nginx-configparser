1b525a934ef51c936d4b19214b2cdad13233e979
Alfred Lucero <alfred.lucero@ucla.edu>
2017-01-16 22:31:42 -0800

    Added simple existing and non-existing file parser tests and string config fixture

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,33 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
+// Nginx String Config Fixture based on lecture example 
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+  bool ParseString(const std::string config_string) {
+    std::stringstream config_stream(config_string);
+    return parser_.Parse(&config_stream, &out_config_);
+  }
+
+  NginxConfigParser parser_;
+  NginxConfig out_config_;
+};
+
+TEST(NginxConfigParserTest, SimpleConfigExistingFile) {
   NginxConfigParser parser;
   NginxConfig out_config;
 
-  bool success = parser.Parse("example_config", &out_config);
+  bool is_file_parsed = parser.Parse("example_config", &out_config);
 
-  EXPECT_TRUE(success);
+  EXPECT_TRUE(is_file_parsed);
 }
+
+TEST(NginxConfigParserTest, SimpleConfigNonExistingFile) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+  
+  bool is_file_parsed = parser.Parse("non_existent_config", &out_config);
+  
+  EXPECT_FALSE(is_file_parsed);
+}
+
